dc62b373e0cc2b99919913d31035b2c8c4346faf
Samoel <samoelmoura@hotmail.com>
2022-07-05 15:02:26 -0300

    my first commit

---
+++ handler.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, Response
+import pandas as pd
+from packages.top_bank import Top_bank
+import os
+
+# initialize API
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+
+def top_bank_predict():
+    df_raw_json = request.get_json()
+    
+    if df_raw_json:
+        if isinstance(df_raw_json, dict):
+            df_raw = pd.DataFrame(df_raw_json, index=[0])
+            
+        else:
+            df_raw = pd.DataFrame(df_raw_json, columns=df_raw_json[0].keys())
+    
+        # instantiate Top_bank_class
+        papeline = Top_bank()
+
+        # data_cleaning
+        df = papeline.data_filtering(df_raw)
+
+        # data_preparation
+        df = papeline.data_preparation(df_raw)
+
+        # get_propensity
+        df = papeline.get_propensity(df, df_raw)
+
+        # return json
+        df = df.to_json(orient='records')
+        return df
+    
+    else:
+        return Response('{}', status=200, mimetype='application/json')
+    
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 5000) # heroku host
+    app.run('0.0.0.0', port=port)
---
+++ top_bank.py
@@ -0,0 +1,44 @@
+import pickle
+import pandas as pd
+import numpy as np
+
+class Top_bank(object):
+    def __init__(self):
+        self.cols_filtering = pickle.load(open('features/cols_filtering.pkl', 'rb'))
+        self.ss_balance = pickle.load(open('features/ss_balance.pkl', 'rb'))
+        self.mm_credit_score = pickle.load(open('features/mm_credit_score.pkl', 'rb'))
+        self.mm_estimatedsalary = pickle.load(open('features/mm_estimatedsalary.pkl', 'rb'))
+        self.map_geography = pickle.load(open('features/map_geography.pkl', 'rb'))
+        self.map_gender = pickle.load(open('features/map_gender.pkl', 'rb'))
+        self.map_numofproducts = pickle.load(open('features/map_numofproducts.pkl', 'rb'))
+        self.tenure_cicle = pickle.load(open('features/tenure_cicle.pkl', 'rb'))
+        self.cols_drop = pickle.load(open('features/cols_drop.pkl', 'rb'))
+        self.cols_drop_split = pickle.load(open('features/cols_drop_split.pkl', 'rb'))
+        self.map_test_id_salary = pickle.load(open('features/map_test_id_salary.pkl', 'rb'))
+        self.model = pickle.load(open('models/xgb_model.pkl', 'rb'))
+    
+    def data_filtering(self, df):
+        df.drop(self.cols_filtering, axis=1, inplace=True)
+        return df
+        
+        
+    def data_preparation(self, df):
+        df['Balance'] = self.ss_balance.transform(df[['Balance']].values)
+        df['CreditScore'] = self.mm_credit_score.transform(df[['CreditScore']].values)
+        df['EstimatedSalary'] = self.mm_estimatedsalary.transform(df[['EstimatedSalary']].values)
+        df['Age'] = np.log1p(df['Age'])
+        df['Geography'] = df['Geography'].map(self.map_geography)
+        df['Gender'] = df['Gender'].map(self.map_gender)
+        df['NumOfProducts'] = df['NumOfProducts'].map(self.map_numofproducts)
+        df['Tenure_sin'] = df['Tenure'].apply(lambda x: np.sin(x* (2*np.pi/self.tenure_cicle)))
+        df['Tenure_cos'] = df['Tenure'].apply(lambda x: np.cos(x* (2*np.pi/self.tenure_cicle)))
+        df.drop('Tenure', axis=1, inplace=True)
+        df.drop(self.cols_drop, axis=1, inplace=True)
+        df.drop(self.cols_drop_split, axis=1, inplace=True)
+        return df
+
+
+    def get_propensity(self, df, df_raw):
+        predict_proba = self.model.predict_proba(df)
+        df_raw['propensity'] = predict_proba[:, 1]
+        return df_raw
